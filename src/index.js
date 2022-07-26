3f69ba44b5fffe72637ed90f2bd06ca50bcfe28f
Lorena Moreno <kmoreno1209@gmail.com>
2022-07-26 18:18:52 -0500

    proyecto 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import cipher from './cipher.js';
-alert('hola')
-console.log(cipher);
+
+//let resultado = document.getElementById("resultado"); // input cuadro descodificación
+
+document.getElementById("input_inicial").addEventListener("click", cifrar);
+
+// Formula = (cipher.encode - 65 + clave) % 26 + 65
+
+function cifrar () {
+    let cifrador = document.getElementById("cifrador").value; // input cifrador
+    let clave = parseInt(document.getElementById("clave").value); // número de posiciones
+    document.getElementById("resultado").value = cipher.encode(clave,cifrador);// para mostrar el resultade cifrar en la casilla desifrar
+    
+}
+
+document.getElementById("input_final").addEventListener("click", descifrar);
+
+function descifrar(){
+    const resultado = document.getElementById("resultado").value; // input cuadro descodificación
+    const clave = parseInt(document.getElementById("clave").value); // número de posiciones
+    document.getElementById("cifrador").value = cipher.decode(clave,resultado); // para mostrar el descifrado
+
+}
+
