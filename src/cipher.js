dd22f071111b5ecb6fab2297f07cb24635e92583
Lorena Moreno <kmoreno1209@gmail.com>
2022-07-26 18:01:00 -0500

    cipher.js listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,32 @@
 const cipher = {
-  // ...
+
+  encode: function (offset, string) {
+    if(!offset || string.length == 0){
+       
+      throw new TypeError("Llena todos los campos")
+  
+    }
+    
+    let salida = "";
+
+    for (let i = 0; i < string.length; i++) {
+      let numeroAsciiO = ((string[i].charCodeAt() - 65 + offset) % 26) + 65;
+      salida += String.fromCharCode(numeroAsciiO);
+    }
+
+    return salida;
+  },
+
+  decode: function (offset, string) {
+    let salida2 = "";
+    for (let i = 0; i < string.length; i++) {
+      let numeroAscii2 = ((string[i].charCodeAt() + 65 - offset) % 26) + 65;
+      console.log(numeroAscii2)
+      salida2 += String.fromCharCode(numeroAscii2);
+      }
+
+    return salida2;
+  },
 };
 
 export default cipher;
